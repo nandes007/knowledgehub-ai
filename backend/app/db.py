37d315666ecc701171d314b1f6b8026c7a1d87b8
Nandes <nandessimanjuntak1803@gmail.com>
2026-07-27 12:57:33 +0700

    fix linter

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,11 +1,13 @@
 from collections.abc import Generator
 
-from sqlalchemy import Engine, event
+from sqlalchemy import Engine
 from sqlmodel import Session, SQLModel, create_engine
 
 from app.config import settings
 
 engine = create_engine(settings.database_url)
+
+
 
 def create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
